26aa508d77ee0c383a9253f8531cbb99639f239f
pepelarana44344 <159673653+lFunkwnon01@users.noreply.github.com>
2026-06-14 23:06:09 -0500

    Update BuscarAlumno.py

--- BuscarAlumno.py
+++ BuscarAlumno.py
@@ -1,25 +1,22 @@
 import boto3
 
-
 def lambda_handler(event, context):
-    # Entrada - parámetros vienen del PATH de la URL
+    # Entrada - vienen del PATH de la URL
     tenant_id = event['path']['tenant_id']
     alumno_id = event['path']['alumno_id']
-
+    
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
-
     response = table.get_item(
         Key={
             'tenant_id': tenant_id,
             'alumno_id': alumno_id
         }
     )
-
     alumno = response.get('Item', None)
-
-    # Salida (json)
+    
+    # Salida
     if alumno:
         return {
             'statusCode': 200,
